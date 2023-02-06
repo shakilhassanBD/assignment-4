b2d78985f4620274bcb91b63c27b40f81236eecf
shakilhassanBD <hasanshakil564@gmail.com>
2023-02-07 00:20:12 +0600

    third problem is solved

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 //First problem is given below......
 
-function mindGame(positivenumber) {
-    const multiplication = positivenumber *3;
+function mindGame(positiveNumber) {
+    const multiplication = positiveNumber * 3;
     const summation = multiplication + 10;
-    const division = summation /2 ;
+    const division = summation / 2;
     const result0 = division - 5;
     console.log(result0);
 
@@ -16,14 +16,35 @@ const sendNumber = mindGame(givenNumber);
 //second problem is given below......
 
 
-function  evenOdd(evenOrOdd){
+function evenOdd(evenOrOdd) {
     const result1 = evenOrOdd.length;
-    if(result1 %2 == 0){
+    if (result1 % 2 == 0) {
         console.log('even');
     }
-    else{
+    else {
         console.log('odd');
     }
 }
 const givenString = 'Batch-7';
 const sendString = evenOdd(givenString);
+
+
+//third problem is given below......
+
+function isLGSeven(parameter) {
+    let difference = parameter - 7;
+    let doubleOFInput = parameter *2;
+    if (difference < 7) {
+        console.log(difference);
+
+        return difference;
+    }
+
+    else {
+        console.log(doubleOFInput);
+        
+        return doubleOFInput;
+    }
+}
+const givenValue = 15;
+const sendValue = isLGSeven(givenValue);

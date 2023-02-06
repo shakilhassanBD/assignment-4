4bca208f618964ec1d3d7f111a9728467ee05e4b
shakilhassanBD <hasanshakil564@gmail.com>
2023-02-06 23:49:07 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+//First problem is given below......
+
+function mindGame(positivenumber) {
+    const multiplication = positivenumber *3;
+    const summation = multiplication + 10;
+    const division = summation /2 ;
+    const result0 = division - 5;
+    console.log(result0);
+
+    return result0;
+}
+
+const givenNumber = 50;
+const sendNumber = mindGame(givenNumber);
+
+//second problem is given below......

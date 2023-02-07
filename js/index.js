35d7ba194034e2afe62647cbad92419f48953638
shakilhassanBD <hasanshakil564@gmail.com>
2023-02-07 22:34:46 +0600

    all complet

--- js/index.js
+++ js/index.js
@@ -1,77 +1,78 @@
-// //First problem is given below......
+//First problem solution is given below......
 
-// function mindGame(positiveNumber) {
-//     const multiplication = positiveNumber * 3;
-//     const summation = multiplication + 10;
-//     const division = summation / 2;
-//     const result0 = division - 5;
-//     console.log(result0);
+function mindGame(positiveNumber) {
+    const multiplication = positiveNumber * 3;
+    const summation = multiplication + 10;
+    const division = summation / 2;
+    const result0 = division - 5;
+    console.log(result0);
 
-//     return result0;
-// }
+    return result0;
+}
 
-// const givenNumber = 33;
-// const sendNumber = mindGame(givenNumber);
+const givenNumber = 33;
+const sendNumber = mindGame(givenNumber);
 
 // //second problem is given below......
 
 
-// function evenOdd(evenOrOdd) {
-//     const result1 = evenOrOdd.length;
-//     if (result1 % 2 == 0) {
-//         console.log('even');
-//     }
-//     else {
-//         console.log('odd');
-//     }
-// }
-// const givenString = 'Batch-7';
-// const sendString = evenOdd(givenString);
+function evenOdd(evenOrOdd) {
+    const result1 = evenOrOdd.length;
+    if (result1 % 2 == 0) {
+        console.log('even');
+    }
+    else {
+        console.log('odd');
+    }
+}
+const givenString = 'Batch-7';
+const sendString = evenOdd(givenString);
 
 
-// //third problem is given below......
+//third problem solution is given below......
 
-// function isLGSeven(parameter) {
-//     let difference = parameter - 7;
-//     let doubleOFInput = parameter *2;
-//     if (difference < 7) {
-//         console.log(difference);
+function isLGSeven(parameter) {
+    let difference = parameter - 7;
+    let doubleOFInput = parameter *2;
+    if (difference < 7) {
+        console.log(difference);
 
-//         return difference;
-//     }
+        return difference;
+    }
 
-//     else {
-//         console.log(doubleOFInput);
+    else {
+        console.log(doubleOFInput);
 
-//         return doubleOFInput;
-//     }
-// }
-// const givenValue = 15;
-// const sendValue = isLGSeven(givenValue);
+        return doubleOFInput;
+    }
+}
+const givenValue = 15;
+const sendValue = isLGSeven(givenValue);
 
 
-// //fourth problem is given below......
+//fourth problem solution is given below......
 
-// function findingBadData(data) {
-//     const goodData = [];
-//     const badData = [];
-//     for (let i = 0; i < data.length; i++) {
-//         const index = i;
-//         const elements = data[index];
-//         if (elements < 0) {
-//             badData.push(elements);
-//             console.log(elements, ' :Bad Data,');
+function findingBadData(data) {
+    let badData = [];
+    for (let i = 0; i < data.length; i++) {
+        let elements = data[i];
+        if (elements < 0) {
+            badData.push(elements);
+        }
+    }
+    if (badData.length > 0) {
+        console.log(badData.length);
+    }
+    else {
+        console.log(0);
+    }
+    return badData.length;
+}
 
-//         }
-//         else if (elements >= 0) {
-//             goodData.push(elements);
-//             console.log(elements, ' :Good Data');
-//         }
-//     }
-//     return badData;
-// }
-// const arrayOfData = [5, 10, 15, -4, -8, -12];
-// const sendData = findingBadData(arrayOfData);
+const dataList = [2, -5, -7, -13];
+const result = findingBadData(dataList);
+
+
 
 //fifth problem solution given below......
 
@@ -79,7 +80,7 @@ function gemsToDiamond(firstFriendGem, secondFriendGem, thirdFriendGem) {
     const firstFriendDiamond = firstGem * 21;
     const secondFriendDiamond = secondGem * 32;
     const thirdFriendDiamond = thirdGem * 43;
-    const totalDiamond = firstFriendDiamond + secondFriendDiamond + thirdFriendDiamond;
+    const totalDiamond = firstFriendDiamond + secondFriendDiamond +    thirdFriendDiamond;
 
     let existingDiamond = totalDiamond - 2000;
     if (totalDiamond > 1000*2) {
